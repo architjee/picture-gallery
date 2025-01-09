b6f38db38838cf5605ac65f5704cb9275ed4dc4e
Archit Jain <architjee@gmail.com>
2025-01-09 12:45:39 +0530

    feat(tailwind) migrate Tailwind configuration to TypeScript; add icon plugins and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons';
+
 export default {
   content: [
     "./index.html",
@@ -9,11 +11,14 @@ export default {
   },
   plugins: [
     require('daisyui'),
+    iconsPlugin({
+      collections: getIconCollections(["heroicons"]),
+    }),
   ],
   daisyui: {
     themes: false,
     darkTheme: "dark", // name of one of the included themes for dark mode
     prefix: "d-", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
   },
-}
+} satisfies Config;
 
