469b75e0d65e91f8ab0734fee16150f10c3fb09c
Archit Jain <architjee@gmail.com>
2025-01-09 13:15:59 +0530

    feat(App) add TheFooter component and integrate into App.vue layout; adjust structure for improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue';
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col items-center border-2 border-base-content bg-base-100">
     <NavBar></NavBar>
-    <div>
+    <div class="grow">
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
+    <TheFooter />
   </div>
-  
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
