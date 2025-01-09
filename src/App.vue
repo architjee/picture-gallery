a76a0ce71ae4e8e57d45f391566d73817a6e3b6c
Archit Jain <architjee@gmail.com>
2025-01-09 12:03:26 +0530

    refactor App.vue layout; add flexbox and border styling for improved structure

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="min-h-screen flex flex-col items-center border-2 border-gray-100 justify-center bg-gray-100">
+    <HelloWorld msg="Vite + Vue" />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+  
 </template>
 
 <style scoped>
