162e600bd882ed453e155929c2b11ff92a064c53
Archit Jain <architjee@gmail.com>
2025-01-09 13:28:14 +0530

    feat(HomePage) implement drawer layout and integrate HomePage component into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue';
 import TheFooter from '@/components/TheFooter.vue'
+import HomePage from '@/pages/HomePage.vue'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col items-center border-2 border-base-content bg-base-100">
     <NavBar></NavBar>
     <div class="grow">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <HomePage></HomePage>
     </div>
     <TheFooter />
   </div>
