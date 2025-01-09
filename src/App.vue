fb2b5d1ab4384a2fc765fc255449d2d4e873a652
Archit Jain <architjee@gmail.com>
2025-01-09 12:33:43 +0530

    feat(@ alias) add Navbar component and update App.vue layout; configure TypeScript paths and Vite alias

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col items-center border-2 border-base-content justify-center bg-base-100">
-    <HelloWorld msg="Vite + Vue" />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="min-h-screen flex flex-col items-center border-2 border-base-content bg-base-100">
+    <Navbar></Navbar>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   </div>
   
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+   <div class="d-navbar bg-base-100">
+  <div class="d-navbar-start">
+    <div class="d-dropdown">
+      <div tabindex="0" role="button" class="d-btn d-btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="d-menu d-menu-sm d-dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a class="d-btn d-btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div class="d-navbar-center hidden lg:flex">
+    <ul class="d-menu d-menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div class="d-navbar-end">
+    <a class="d-btn">Button</a>
+  </div>
+</div>
+</template>
+
+<script lang="ts" setup>
+// something
+</script>
+
+<style scoped></style>
