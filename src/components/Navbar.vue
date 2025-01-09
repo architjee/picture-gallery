08c2a6618ac67aae8f8faffcc7bc31877663eff2
Archit Jain <architjee@gmail.com>
2025-01-09 17:44:47 +0530

    feat(Navbar) update navigation items and branding for improved clarity

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,16 +6,16 @@
           <i class="i-heroicons-bars-3"></i>
         </div>
         <ul tabindex="0" class="d-menu d-menu-sm d-dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li><a>Item 3</a></li>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
         </ul>
       </div>
-      <a class="d-btn d-btn-ghost text-xl">daisyUI</a>
+      <a class="d-btn d-btn-ghost text-xl">Arch</a>
     </div>
     <div class="d-navbar-center hidden lg:flex">
       <ul class="d-menu d-menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li><a>Item 3</a></li>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
       </ul>
     </div>
     <ThemeSwitcherButton class="d-navbar-end" />
@@ -24,7 +24,6 @@
 
 <script lang="ts" setup>
 import ThemeSwitcherButton from '@/components/ThemeSwitcherButton.vue'
-// something
 </script>
 
 <style scoped></style>
