a7dff0a42c3f7f5d57706757b5db864efb95b371
Archit Jain <architjee@gmail.com>
2025-01-10 10:58:14 +0530

    feat: enhance AboutPage layout and content for better user experience

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-
+    <div class="grow text-center">
+        <div class="max-w-96 flex h-full flex-col justify-center gap-y-5 mx-auto">
+            <p class="text-xl font-bold">
+                Picsum Photos Gallery application made using Vue, TypeScript and Tailwind
+            </p>
+            <p class="text-lg font-medium italic">
+                The cogs on the right represent settings menu.
+            </p>
+            <p class="text-xl font-light">
+                Both theme and no. of items per page are persisted
+            </p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 </script>
-
-<style scoped>
-
-</style>
